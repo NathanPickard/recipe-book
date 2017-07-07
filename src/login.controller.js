7a30298fb97aa1f8e4b6eafdc3badf2ac3c2390e
Nathan Pickard <nathan.pickard87@gmail.com>
2017-07-06 21:39:42 -0700

    User authentication dependencies and back-end routes started

---
+++ src/login.controller.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var config = require('config.json');
+
+router.get('/', function(req, res) {
+  
+})
