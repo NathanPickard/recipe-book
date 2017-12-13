6604239566290a755a45ebaf1447f4e4008eceef
Nathan Pickard <nathan.pickard87@gmail.com>
2017-12-12 23:28:41 -0800

    localhost and mLab databases connection added

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://heroku_hkf9sswk:du1fjgqf4a6pq2njtom5orndv5@ds135876.mlab.com:35876/heroku_hkf9sswk');
+mongoose.connect('mongodb://heroku_hkf9sswk:du1fjgqf4a6pq2njtom5orndv5@ds135876.mlab.com:35876/heroku_hkf9sswk' || 'mongodb://localhost/recipebook');
 
 // if (process.env.NODE_ENV == "production") {
 //   mongoose.connect(process.env.MONGOLAB_URL);
