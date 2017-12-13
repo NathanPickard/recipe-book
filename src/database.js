1bcd4a773f30d680d4fd40d50b12bb203f4caf97
Nathan Pickard <nathan.pickard87@gmail.com>
2017-12-12 22:35:31 -0800

    mongoose connect for heroku

--- src/database.js
+++ src/database.js
@@ -2,15 +2,17 @@
 
 var mongoose = require('mongoose');
 
-if (process.env.NODE_ENV == "production") {
-  mongoose.connect(process.env.MONGOLAB_URL);
-}
-else {
-  mongoose.connect('mongodb://localhost/recipebook', function (err) {
-    if (err) {
-      console.log('Failed connecting to Mongodb!');
-    } else {
-      console.log('Successfully connected to Mongo!');
-    }
-  });
-}
+mongoose.connect('mongodb://heroku_hkf9sswk:du1fjgqf4a6pq2njtom5orndv5@ds135876.mlab.com:35876/heroku_hkf9sswk'))
+
+// if (process.env.NODE_ENV == "production") {
+//   mongoose.connect(process.env.MONGOLAB_URL);
+// }
+// else {
+//   mongoose.connect('mongodb://localhost/recipebook', function (err) {
+//     if (err) {
+//       console.log('Failed connecting to Mongodb!');
+//     } else {
+//       console.log('Successfully connected to Mongo!');
+//     }
+//   });
+// }
