9fc23071230476f846d99bedf24db30e35a58ee1
Nathan Pickard <nathan.pickard87@gmail.com>
2017-12-12 17:19:19 -0800

    Adding mongodb_URL

--- src/database.js
+++ src/database.js
@@ -2,10 +2,15 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/recipebook', function(err) {
-  if(err) {
-    console.log('Failed connecting to Mongodb!');
-  } else {
-    console.log('Successfully connected to Mongo!');
-  }
-});
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MONGOLAB_URL);
+}
+else {
+  mongoose.connect('mongodb://localhost/recipebook', function (err) {
+    if (err) {
+      console.log('Failed connecting to Mongodb!');
+    } else {
+      console.log('Successfully connected to Mongo!');
+    }
+  });
+}
