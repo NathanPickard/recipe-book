2319075d8372ad6bbb1f38d6ed2d775ae14d524c
Nathan Pickard <nathan.pickard87@gmail.com>
2017-12-21 22:19:04 -0800

    Adding login, register, api/users routes

--- src/app.js
+++ src/app.js
@@ -16,6 +16,12 @@ app.use(session({ secret: config.secret, resave: false, saveUninitialized: true 
 
 app.use('/api', router);
 
+//routes
+app.use('/login', require('./login.controller'));
+app.use('/register', require('./register.controller'));
+app.use('/api/users', require('./api/users.controller'));
+
+
 app.listen(process.env.PORT || 3000, function() {
   console.log("The server is running on port 3000!");
 });
